7fbb91162f79233ebb26d1a9448ac67c479bc154
FrancoDalla <francodalladg@outlook.com>
2025-10-11 02:41:31 +0000

    Varianza

--- varianza.py
+++ varianza.py
@@ -4,12 +4,12 @@ varianza = ssr/(n-2)
 
 
 
-ssr_años = 16934618.872522076
-ssr_rendimiento = 35143399.327570536
+sce_años = 16934618.872522076
+sce_rendimiento = 35143399.327570536
 n = 27
 
-varianza_años = ssr_años/(n-2)
-varianza_rendimiento = ssr_rendimiento/(n-2)
+varianza_años = sce_años/(n-2)
+varianza_rendimiento = sce_rendimiento/(n-2)
 
 print(f"Varianza años: {varianza_años}")
 print(f"Varianza rendimiento: {varianza_rendimiento}")
