04edbb1fd28c547bb8452612954ec4e56d6b3b6e
FrancoDalla <francodalladg@outlook.com>
2025-10-11 02:36:11 +0000

    Varianza

---
+++ varianza.py
@@ -0,0 +1,15 @@
+"""
+varianza = ssr/(n-2)
+"""
+
+
+
+ssr_años = 16934618.872522076
+ssr_rendimiento = 35143399.327570536
+n = 27
+
+varianza_años = ssr_años/(n-2)
+varianza_rendimiento = ssr_rendimiento/(n-2)
+
+print(f"Varianza años: {varianza_años}")
+print(f"Varianza rendimiento: {varianza_rendimiento}")
